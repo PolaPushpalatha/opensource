2333d35bdd3bff1c605bdd95571fa35a136d3fcc
PolaPushpalatha <polapushpalatha23@gmail.com>
2024-11-19 09:19:46 +0530

    Create DND.py

---
+++ VISAA_PREP/DND.py
@@ -0,0 +1,14 @@
+def s():
+    n,m=map(int,input().split())
+    arr=list(map(int,input().split()))
+    
+    num1=0
+    num2=0
+    
+    for num in arr:
+        if num%m==0:
+            num1+=num
+        else:
+            num2+=num
+    print(num1-num2) 
+s()    
