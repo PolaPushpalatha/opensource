67e0afa0fa18a2d15f420231c974039482c5553e
PolaPushpalatha <polapushpalatha23@gmail.com>
2024-11-19 08:35:18 +0530

    Create Triangle pattern 1.py

---
+++ VISAA_PREP/Triangle pattern 1.py
@@ -0,0 +1,12 @@
+def p():
+    n=int(input().strip())
+    num=1
+    
+    for i in range(1,n+1):
+        for j in range(i):
+            print(num,end=" ")
+            num+=1
+        print()
+p()        
+            
+            
